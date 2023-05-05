1a52884c0bff74db7e3279507bdfdcc7e2649cda
WalterPimentel <70056787@continental.edu.pe>
2023-05-04 21:57:50 -0500

    v6 (Eloquent)

---
+++ app/Models/responsables_cpi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class responsables_cpi extends Model
+{
+    use HasFactory;
+
+    protected $table = "nombreTablas";
+}
